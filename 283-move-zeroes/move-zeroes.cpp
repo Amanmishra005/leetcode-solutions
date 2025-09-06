4d1093273f89f546efc061ec72b2826710938919
Aman Mishra <amanmishra2884@gmail.com>
2025-09-06 13:35:29 +0530

    Time: 0 ms (100.00%) | Memory: 23.7 MB (81.88%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -3,9 +3,9 @@ public:
     void moveZeroes(vector<int>& nums) {
         int n = nums.size();
         int i=0;
-         if(n<=1) return;
+         if(n<1) return;
 
-        for(int j=0;j<n;j++){
+        for(int j=0;j<n;j++){ //j=0 to check if array has only one element
             if(nums[j]!=0) {
                 nums[i] = nums[j];
                 i++;
