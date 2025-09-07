d8346e9b9c96b1254fb7ba6283bf7d3c8c1cf049
Aman Mishra <amanmishra2884@gmail.com>
2025-09-08 02:12:23 +0530

    Time: 0 ms (100.00%) | Memory: 20.7 MB (58.12%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -2,11 +2,11 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         int n = nums.size();
-        int result = 0;   
+        int result = 0;                 //different element will return on comparing
         for (int i = 0; i < n; i++) {
             result = result ^ nums[i];   //0^0 = 0 || 1^1 = 0 comparing with result i.e 0
         }                                 //1^0 = 0 || 0^1 = 1
-
+                                        //(1 ^ 1) ^ (2 ^ 2) ^ 4
         return result;
     }
 };
