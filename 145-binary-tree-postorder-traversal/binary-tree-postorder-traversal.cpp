0c286f8ed3626b8e6027fd193b1677e4821347f1
Aman Mishra <amanmishra2884@gmail.com>
2025-09-14 20:19:32 +0530

    Time: 2 ms (5.58%) | Memory: 10.8 MB (92.35%) - LeetSync

--- 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.cpp
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.cpp
@@ -11,16 +11,38 @@
  */
 class Solution {
 public:
-void postOrder(TreeNode* root,vector<int>& ans){
-    if(root==NULL) return;
-    postOrder(root->left,ans);
-    postOrder(root->right,ans);
-    ans.push_back(root->val);
+//void postOrder(TreeNode* root,vector<int>& ans){
+//     if(root==NULL) return;
+//     postOrder(root->left,ans);
+//     postOrder(root->right,ans);
+//     ans.push_back(root->val);
     
-}
-    vector<int> postorderTraversal(TreeNode* root) {
-        vector<int> ans;
-        postOrder(root,ans);
-        return ans;
+// }
+//     vector<int> postorderTraversal(TreeNode* root) {
+//         vector<int> ans;
+//         postOrder(root,ans);
+//         return ans;
+//     }
+
+//Iterative method ->just push left then right and reverse the vector
+ vector<int> postorderTraversal(TreeNode* root){
+    vector<int> ans;
+        if(root==NULL) return ans;
+        stack<TreeNode*>st;
+        st.push(root);
+        while(!st.empty()){
+            TreeNode* temp = st.top();
+            st.pop();
+            ans.push_back(temp->val);
+            //right first for reversal order in the stack
+             if(temp->left!=NULL) st.push(temp->left);
+            if(temp->right!=NULL) st.push(temp->right);
+           
+        }        
+         reverse(ans.begin(),ans.end());
+         return ans;
     }
+
+
+
 };
