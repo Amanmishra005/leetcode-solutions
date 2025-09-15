1637914b660f49534643f13bbe3cb20a3ad5ed5e
Aman Mishra <amanmishra2884@gmail.com>
2025-09-15 18:36:09 +0530

    Time: 0 ms (100.00%) | Memory: 14.9 MB (60.95%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
@@ -14,28 +14,18 @@ public:
 int levels(TreeNode* root){
     if(root==NULL) return 0;
     return 1+max(levels(root->left),levels(root->right));
-};
-void nthLevel(TreeNode* root,int currL,int level,vector<int>& ans){
+}
+void preOrder(TreeNode* root,vector<int>& ans,int level){
     if(root==NULL) return;
-    if(currL==level){
-       ans[currL] = root->val;  //last node at that level will get stored since we are we r visting each node at that level so the last node that llevel will get stored
-        return; 
-    }
-    nthLevel(root->left,currL+1,level,ans);
-    nthLevel(root->right,currL+1,level,ans);
+    ans[level] =root->val;
+    preOrder(root->left,ans,level+1);
+    preOrder(root->right,ans,level+1);
 }
-void LevelOrder(TreeNode* root,vector<int>& ans){    //prints all nodes at the given level in a binary tree 
-    int n = ans.size();
-    for(int i=0;i<n;i++){
-        nthLevel(root,0,i,ans);
-       
-    }
-}
+
     vector<int> rightSideView(TreeNode* root) {
         vector<int> ans(levels(root),0);   //     size,intial value->0
-       LevelOrder(root,ans);
-       return ans;
-
+        preOrder(root,ans,0);
+        return ans;
        
 
     }
