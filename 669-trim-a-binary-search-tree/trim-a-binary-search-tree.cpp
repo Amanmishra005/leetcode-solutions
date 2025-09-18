ce1afef74b96faed3d71796da46e93108e425e83
Aman Mishra <amanmishra2884@gmail.com>
2025-09-18 21:55:08 +0530

    Time: 0 ms (100.00%) | Memory: 24.9 MB (56.86%) - LeetSync

--- 669-trim-a-binary-search-tree/trim-a-binary-search-tree.cpp
+++ 669-trim-a-binary-search-tree/trim-a-binary-search-tree.cpp
@@ -4,17 +4,17 @@ public:
     void trim(TreeNode* root,int lo,int hi){
         if(root==NULL) return;
         while(root->left!=NULL){
-            if(root->left->val < lo) root->left = root->left->right;
-            else if(root->left->val > hi) root->left = root->left->left;
+            if(root->left->val < lo) root->left = root->left->right; //to get big value
+            else if(root->left->val > hi) root->left = root->left->left;    //to get small value left->left
             else break;
         }
         while(root->right!=NULL){
-            if(root->right->val > hi) root->right = root->right->left;
-            else if(root->right->val < lo) root->right= root->right->right;
+            if(root->right->val > hi) root->right = root->right->left;  //less
+            else if(root->right->val < lo) root->right= root->right->right; //more
             else break;
         }
-        trim(root->left,lo,hi);
-        trim(root->right,lo,hi);
+        trim(root->left,lo,hi); //for every node onleft 
+        trim(root->right,lo,hi);    //for eveyr node in right
         
     }
     TreeNode* trimBST(TreeNode* root, int lo, int hi) {
