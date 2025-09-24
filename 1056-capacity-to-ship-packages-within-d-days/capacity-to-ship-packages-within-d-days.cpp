37f56218bc3ef9b1d7ef25e244abe4d87ce26b18
Aman Mishra <amanmishra2884@gmail.com>
2025-09-24 19:47:37 +0530

    Time: 7 ms (91.45%) | Memory: 35 MB (84.95%) - LeetSync

---
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    bool check(int mid,vector<int>& weights,int days){
+        int n = weights.size();
+        int m = mid;            //max capacity 
+        int count = 1;          //days we are counting on the basis of mid taken 
+        for(int i=0;i<n;i++){
+            if(m>=weights[i]){
+                m -= weights[i];        // put this package in current ship
+            }
+            else{
+                count++;        //need one more day 
+                m = mid ;       // reset ship capacity next day 
+                m -= weights[i];    // load current package into the new ship 
+            }
+        }
+        if(count> days) return false;
+        else return true;
+    }
+    int shipWithinDays(vector<int>& weights, int days) {
+        int n = weights.size();
+        int max = INT_MIN;
+        int sum = 0;
+        for(int i=0;i<n;i++){
+            if(max < weights[i]) max = weights[i];
+            sum += weights[i];
+        }
+        int lo = max;
+        int hi = sum;
+        int minCapacity =  sum;
+        while(lo<=hi){
+            int mid = lo + (hi-lo)/2;
+            if(check(mid,weights,days)){    //default true else false 
+                minCapacity = mid;
+                hi = mid-1;
+            }
+            else lo = mid+1;
+        }
+        return minCapacity;
+    }
+};
