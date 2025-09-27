ca6064321fbd4cd273a0d552be9651fa6247fb17
Aman Mishra <amanmishra2884@gmail.com>
2025-09-27 22:13:35 +0530

    Time: 0 ms (100.00%) | Memory: 7.6 MB (100.00%) - LeetSync

--- 795-k-th-symbol-in-grammar/k-th-symbol-in-grammar.cpp
+++ 795-k-th-symbol-in-grammar/k-th-symbol-in-grammar.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int kthGrammar(int n, int k) {
-        if(n==1) return 0;
+        if(n==1) return 0;  //starts with 0
         if(k%2==0){     //checking for even case
             int prevAns =  kthGrammar(n-1,k/2); //root index
             if(prevAns==0) return 1;        //flip 
