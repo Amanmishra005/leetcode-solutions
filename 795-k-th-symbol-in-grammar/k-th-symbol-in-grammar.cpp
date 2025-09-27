32c1e1e56be1a92be69963449b3f471157a1f328
Aman Mishra <amanmishra2884@gmail.com>
2025-09-27 22:12:53 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (49.77%) - LeetSync

---
+++ 795-k-th-symbol-in-grammar/k-th-symbol-in-grammar.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int kthGrammar(int n, int k) {
+        if(n==1) return 0;
+        if(k%2==0){     //checking for even case
+            int prevAns =  kthGrammar(n-1,k/2); //root index
+            if(prevAns==0) return 1;        //flip 
+            else return 0;
+        }
+        else{       //k%2!=0
+            int prevAns =  kthGrammar(n-1,k/2+1);       //root odd index
+            return prevAns;
+        }
+        return k;
+    }
+};
