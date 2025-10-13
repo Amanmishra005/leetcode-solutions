254aaf90021b42ea04858a25e8f2e05fb76f2c6d
Aman Mishra <amanmishra2884@gmail.com>
2025-10-13 18:17:17 +0530

    Time: 2 ms (55.44%) | Memory: 7.8 MB (93.30%) - LeetSync

--- 278-first-bad-version/first-bad-version.cpp
+++ 278-first-bad-version/first-bad-version.cpp
@@ -7,7 +7,7 @@ public:
         int lo = 1,hi  = n;
         while(lo<hi){
             int mid = lo + (hi-lo)/2;
-            if(isBadVersion(mid)) hi  = mid;
+            if(isBadVersion(mid)) hi  = mid; //if found then before mid all r bad versions so shrink hi to mid 
             else lo = mid+1;       //till mid version is good 
         }
         return lo;
