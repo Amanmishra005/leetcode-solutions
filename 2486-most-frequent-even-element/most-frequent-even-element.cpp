51f496724bb6bb3a0ebf74b41a32d723dbb4daba
Aman Mishra <amanmishra2884@gmail.com>
2025-10-27 18:18:46 +0530

    Time: 32 ms (36.58%) | Memory: 41.7 MB (88.40%) - LeetSync

--- 2486-most-frequent-even-element/most-frequent-even-element.cpp
+++ 2486-most-frequent-even-element/most-frequent-even-element.cpp
@@ -8,7 +8,7 @@ public:
         }
         if(mp.empty()) return -1;
         int maxfreq = -1,ans = -1; 
-        for(auto &x: mp){
+        for(auto x: mp){
             int ele = x.first,freq = x.second;
             if(freq > maxfreq || (freq== maxfreq && ele < ans)){    //if freq same then take the smaller ele
                 maxfreq = freq;
