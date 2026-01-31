d8a4a4d1b0916134576eb0c0f820c3aa0ce9ee3b
Aman Mishra <amanmishra2884@gmail.com>
2026-01-31 19:12:37 +0530

    Time: 59 ms (23.87%) | Memory: 28.8 MB (99.13%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -6,9 +6,9 @@ public:
    vector<vector<int>> v;
    for(int i=0;i<n;i++){
     if(i>0 && nums[i]==nums[i-1]) continue;
-    int l = i+1,r=n-1;
+    int l = i+1,r = n-1;
     while(l<r){
-        int sum = nums[i] + nums[l] + nums[r];
+        int sum  = nums[i] +  nums[l]  +  nums[r];
         if(sum==0){
             v.push_back({nums[i],nums[l],nums[r]});
             while(l<r && nums[l]==nums[l+1]) l++;
