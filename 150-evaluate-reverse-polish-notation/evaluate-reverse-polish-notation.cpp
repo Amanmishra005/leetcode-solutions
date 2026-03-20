77d904afd74f1558157c6da0cd1835e8f320c08c
Aman Mishra <amanmishra2884@gmail.com>
2026-03-20 20:25:48 +0530

    Time: 0 ms (100.00%) | Memory: 17.1 MB (66.23%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.cpp
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.cpp
@@ -1,22 +1,29 @@
 class Solution {
 public:
     int evalRPN(vector<string>& tokens) {
-        while(tokens.size() > 1){
-            for(int i=0;i<tokens.size();i++){
-                if(tokens[i]=="+"|| tokens[i]=="-" || tokens[i]=="*" || tokens[i] == "/"){
-                    int a = stoi(tokens[i-2]);
-                    int b = stoi(tokens[i-1]);
-                    int result = 0;
-                    if(tokens[i]=="+") result = a+ b;
-                    else if(tokens[i]=="-") result = a-b;
-                    else if(tokens[i]=="*") result = a*b;
-                    else if(tokens[i]=="/") result =  a/b;
-                    tokens.erase(tokens.begin() + i- 2,tokens.begin() + i +1);
-                    tokens.insert(tokens.begin() + i-2,to_string(result));
-                    break;
-                }
+        stack<int> st;
+        for(string &c : tokens){
+            if(c== "+"){
+                int a = st.top(); st.pop();
+                int b = st.top(); st.pop();
+                st.push(b+a);
             }
+            else if(c=="-"){
+                int a = st.top(); st.pop();
+                int b = st.top(); st.pop();
+                st.push(b-a);
+            }
+            else if(c=="*"){
+                int a = st.top(); st.pop();
+                int b = st.top(); st.pop();
+                st.push(b*a);
+            }else if(c=="/"){
+                int a = st.top();st.pop();
+                int b  = st.top();st.pop();
+                st.push(b/a);
+            }
+            else st.push(stoi(c));
         }
-        return stoi(tokens[0]);
+        return st.top();
     }
 };
