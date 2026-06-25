bbae829b5078ae9decda9297449b9ad19f2fa7bf
Aman Mishra <amanmishra2884@gmail.com>
2026-06-25 23:38:32 +0530

    Time: 39 ms (62.90%) | Memory: 67.7 MB (73.33%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.cpp
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
-typedef pair<int,vector<int>> piv;  //vector is for coordinates i.e in pairs
-    vector<vector<int>> kClosest(vector<vector<int>>& arr, int k) {
-        priority_queue<piv> max; //
-        for(auto v: arr){       //catching vector here 
-            int x = v[0] , y = v[1];        //coordinate x-y
-            int dis = x*x + y*y;
-            max.push({dis,v});  //distance and coordinates from  origin point i.e the vector v itself
-            if(max.size()>k) max.pop();
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        priority_queue<pair<int , pair<int,int>>> maxH;
+        for(auto& point : points){
+            int dist = point[0] * point[0] + point[1] * point[1];
+            maxH.push({dist , {point[0] , point[1]}});
+            if(maxH.size() > k){
+                maxH.pop();
+            }
         }
-        vector<vector<int>> ans;
-        while(!max.empty()){
-            ans.push_back( max.top().second);   //return coordinate pairs
-            max.pop();
-        }
+            vector<vector<int>> ans;
+            while(maxH.size()>0){
+                ans.push_back({maxH.top().second.first,maxH.top().second.second});
+                maxH.pop();
+            }
         return ans;
     }
 };
