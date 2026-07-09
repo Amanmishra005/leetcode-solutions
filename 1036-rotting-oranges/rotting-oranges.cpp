961e5505cdb72bac394df28bdd7d9184599ccc12
Aman Mishra <amanmishra2884@gmail.com>
2026-07-09 20:45:28 +0530

    Time: 0 ms (100.00%) | Memory: 16.7 MB (86.37%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,47 +1,47 @@
 class Solution {
 public:
-
     int orangesRotting(vector<vector<int>>& grid) {
-        int m = grid.size();
-        int n = grid[0].size();
-        queue<pair<pair<int,int> , int>> q; //{row,col} , time
-        int vis[m][n];
-        int cntfresh = 0;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
+        int m =  grid.size();
+        int n =  grid[0].size();
+        queue<pair<pair<int,int> , int>> q;
+          int vis[m][n];
+           int cntfresh = 0;
+           for(int i=0;i<m;i++){
+            for(int j = 0;j<n;j++){
                 if(grid[i][j]==2){
                     q.push({{i,j},0});
                     vis[i][j] = 2;
-                }
-                else{
+                    
+                }else {
                     vis[i][j] = 0;
+                    
                 }
-                if(grid[i][j] == 1 ) cntfresh++;
+                if(grid[i][j]==1) cntfresh++;
             }
-        }
-        int totalTime = 0;
-        int delrow[] = {-1,0,1,0};
-        int delcol[] = {0,-1,0,1};
-        int cnt = 0;
-        while(q.size()!=0){
-            int row =  q.front().first.first;
-            int col = q.front().first.second;
-            int time = q.front().second;
-            
+           }
+           int tm = 0;
+           int delrow[] = {-1,0,1,0};
+            int delcol[] = {0,1,0,-1};
+            int fresh = 0;
+           while(!q.empty()){
+            int r = q.front().first.first;
+            int c = q.front().first.second;
+            int rottentime = q.front().second;
+            tm  = max(tm , rottentime);
             q.pop();
-            for(int i=0;i<4;i++){
-                int nrow = row + delrow[i];
-                int ncol = col + delcol[i];
-                if(nrow>=0 && nrow<m && ncol >= 0 && ncol<n && vis[nrow][ncol]==0 && grid[nrow][ncol]==1){
-                    q.push({{nrow,ncol} , time+1});
+            for(int k = 0;k<4;k++){
+                int nrow = r  + delrow[k];
+                int ncol = c + delcol[k];
+                if(nrow>=0 && nrow<m && ncol>=0 && ncol<n && grid[nrow][ncol]==1 && vis[nrow][ncol]==0){
+                    fresh++;
+                    q.push({{nrow,ncol} , rottentime + 1});
                     vis[nrow][ncol] = 2;
-                    cnt++;
                 }
             }
-            totalTime = max(totalTime , time);
-        }
-        if(cnt != cntfresh) return -1; //all converted or not check 
-        return totalTime;
-
+            tm  = max(tm , rottentime);
+           }
+           if(cntfresh != fresh) return -1;
+           return tm;
     }
+    
 };
