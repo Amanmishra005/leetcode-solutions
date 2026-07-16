ac0c41aba251fddeb861b2b1d070d39b41337f2c
Aman Mishra <amanmishra2884@gmail.com>
2026-07-16 13:57:11 +0530

    Time: 0 ms (100.00%) | Memory: 13.6 MB (27.06%) - LeetSync

--- 684-redundant-connection/redundant-connection.cpp
+++ 684-redundant-connection/redundant-connection.cpp
@@ -1,31 +1,42 @@
 class Solution {
 public:
-bool dfs(vector<vector<int>>& adj , int currnode  , int destnode , vector<bool>& vis){
-    vis[currnode] = true;
-    if(currnode ==destnode) return true;
-    for(auto &node : adj[currnode]){
-        if(!vis[node]){
-            if(dfs(adj , node , destnode , vis)) return true;
-        }
+vector<int> parent;
+vector<int> rank;
+int find(int x){
+    if(parent[x] == x) return x;
+    return parent[x] = find(parent[x]);
+}
+void Union(int x , int y){
+    int parent_x = find(x);
+    int parent_y = find(y);
+    if(parent_x == parent_y) return;
+    if(rank[parent_x] > rank[parent_y]){
+        parent[parent_y] = parent_x;
     }
-    return false;
+    else if(rank[parent_x] < rank[parent_y]){
+        parent[parent_x] = parent_y;
+    }else{
+        parent[parent_y] = parent_x;
+        rank[parent_x]++;
+    }
 }
     vector<int> findRedundantConnection(vector<vector<int>>& edges) {
         int n = edges.size();
+        parent.resize(n+1);
+        rank.resize(n+1);
+        for(int i = 0;i<n;i++){
+            parent[i] = i;
+            rank[i] = 1;
+        }
         vector<vector<int>> adj(n+1);
         for(auto &edge : edges){
             int u = edge[0];
             int v = edge[1];
-            vector<bool> vis(n+1);
-            if(!adj[u].empty() && !adj[v].empty()){
-                if(dfs(adj , u , v , vis)){
-                    return edge;
-                }
+            if(find(u)==find(v)){
+                return edge;
             }
-                 adj[u].push_back(v);
-            adj[v].push_back(u);           
+            Union(u , v);
         }
         return {};
-
     }
 };
