88493d0415fa17981dd78778b195f3773e8f2b27
Aman Mishra <amanmishra2884@gmail.com>
2026-07-19 03:13:28 +0530

    Time: 31 ms (27.93%) | Memory: 23.6 MB (86.07%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -1,39 +1,34 @@
 class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        int m = grid.size();
+        int  m =  grid.size();
         int n = grid[0].size();
-        if (grid[0][0] == 1 || grid[m-1][n-1]==1)
-            return -1;
-        queue<pair<int,int>> q;
-        vector<vector<int>> vis(m, vector<int>(n, 0));
-        q.push({0,0});
-        vis[0][0] = 1;
-        int level = 1;
-                int delrow[] = {-1,-1,-1,0,0,1,1,1};
+        if(grid[0][0]==1 || grid[m-1][n-1]==1) return -1;
+        vector<vector<int>> result(m , vector<int>(n , INT_MAX));
+        priority_queue<pair<int, pair<int,int>> , vector<pair<int, pair<int,int>>> , greater<pair<int , pair<int,int>>>> pq;
+        pq.push({0, {0,0}});
+        result[0][0] = 0;
+        int delrow[] = {-1,-1,-1,0,0,1,1,1};
         int delcol[] = {-1,0,1,-1,1,-1,0,1};
-        while(!q.empty()){
-            int sz = q.size();
-            while(sz--){            //inorder to process only the current level node at a time 
-                auto currnode = q.front();
-                q.pop();
-                int x = currnode.first;
-                int y = currnode.second;
-                if(x==m-1 && y==n-1) return level;
-                for(int i=0;i<8;i++){
-                    int x_ = x + delrow[i];
-                    int y_ = y + delcol[i];
-                    if(x_ >= 0 && x_<m && y_>=0 && y_<n){
-                        if(vis[x_][y_]==0 && grid[x_][y_]==0){
-                            q.push({x_,y_});
-                            vis[x_][y_] = 1;
-                        }
-                    }
+        while(!pq.empty()){
+            int d = pq.top().first;
+            pair<int,int> node = pq.top().second;
+            int x =  node.first;
+            int y = node.second;
+            pq.pop();
+            for(int i=0;i<8;i++){
+                int x_ = x + delrow[i];
+                int y_ = y + delcol[i];
+                int dist = 1;
+                if(x_ >= 0 && x_ < m && y_>= 0 && y_<n && grid[x_][y_]==0  && d+ dist< result[x_][y_]){
+                    pq.push({d+dist , {x_ , y_}});
+                    result[x_][y_] = d+dist;
                 }
             }
-            level++;
-        }
-        return -1;
+            
 
+        }
+        if(result[m-1][n-1]== INT_MAX) return -1;
+        return result[m-1][n-1] + 1;
     }
 };
