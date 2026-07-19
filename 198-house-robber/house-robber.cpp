ca1ba70d02fca57e001502cd29f3cd280faad031
Aman Mishra <amanmishra2884@gmail.com>
2026-07-20 01:57:19 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (89.89%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -3,13 +3,16 @@ public:
     int rob(vector<int>& nums) {
         int  n = nums.size();
         if(n==1) return nums[0];
-        vector<int> t(n+1 , 0);
-        t[1] = nums[0];         //only one house then 
+        //const space 
+        int prevprev = 0;
+        int prev = nums[0];         //only one house then 
         for(int i=2;i<=n;i++){
-            int steal = nums[i-1] + t[i-2];
-            int skip = t[i-1];
-            t[i] = max(steal , skip);
+        int steal = nums[i-1] + prevprev;//current house looting i-1  + amount looted til now not the adjacent one but i.e i-2
+            int skip = prev; //if not looted current one take profit till the adjacent house 
+            int temp = max(steal , skip);
+            prevprev = prev;
+            prev =  temp;
         }
-        return t[n];
+        return prev;
     }
 };
