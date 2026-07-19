81aa1ca385f35565885a347829844f52a04cb9d6
Aman Mishra <amanmishra2884@gmail.com>
2026-07-20 02:14:50 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (74.30%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
+    int solve(vector<int>& nums , int l ,int r){
+        int n =nums.size();
+        int prevprev = 0;
+        int prev = 0;
+        for(int i=l;i<=r;i++){
+            int steal = nums[i] + prevprev;
+            int skip = prev;
+            int temp = max(steal , skip);
+            prevprev = prev;
+            prev = temp;
+        }
+        return prev;
+    }
     int rob(vector<int>& nums) {
-        int n = nums.size();
+        int n  = nums.size();
         if(n==1) return nums[0];
-        vector<int> t(n+1 , 0);
-        t[1] = nums[0];
-        for(int i=1;i<=n-1;i++){
-            int steal = nums[i-1] + (i-2>=0 ? t[i-2] : 0);
-            int skip = t[i-1];
-            t[i] = max(steal , skip);
-        }
-        int withoutlast = t[n-1];
-        t.clear();
-        
-        t[1]  = 0; //skipped first house
-        for(int i=2;i<=n;i++){
-            int steal = nums[i-1] + t[i-2];
-            int skip = t[i-1];
-           t[i] = max(steal , skip);
-        }
-        int withlast = t[n];
-        return max(withoutlast , withlast);
+        if(n==2) return max(nums[0],nums[1]);
+        int taking_house1 = solve(nums ,0 , n-2);
+
+        int not_taking_house1 = solve(nums ,1 , n-1);
+        return max(taking_house1,not_taking_house1);
     }
 };
