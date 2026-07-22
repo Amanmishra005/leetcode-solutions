f6b886ac24528cccfa810b910276cd522c96a469
Aman Mishra <amanmishra2884@gmail.com>
2026-07-22 17:35:25 +0530

    Time: 266 ms (40.08%) | Memory: 157 MB (54.43%) - LeetSync

--- 2022-maximum-alternating-subsequence-sum/maximum-alternating-subsequence-sum.cpp
+++ 2022-maximum-alternating-subsequence-sum/maximum-alternating-subsequence-sum.cpp
@@ -1,22 +1,12 @@
 class Solution {
 public:
-    int n;
-    long long t[100001][2];
-    long long solve(int idx , vector<int>& nums , bool flag){
-        if(idx>=n) return 0;
-
-        long long val = nums[idx];
-        if(t[idx][flag]!=-1) return t[idx][flag];
-        if(flag ==false){
-            val = -val;
-        } 
-        long long skip = solve(idx +1 , nums , flag);
-        long long take = solve(idx+1 , nums , !flag) + val;
-        return t[idx][flag] = max(skip,take);
-    }
     long long maxAlternatingSum(vector<int>& nums) {
-         n = nums.size();
-         memset(t , -1, sizeof(t));
-        return solve(0 , nums , true);
+        int n = nums.size();
+        vector<vector<long long>> t(n+1 , vector<long long>(2,0)); 
+        for(int i=1;i<n+1;i++){
+            t[i][0] = max(t[i-1][1] - nums[i-1] , t[i-1][0]); //0 -> even-> odd on taking 
+            t[i][1] = max(t[i-1][0] + nums[i-1] , t[i-1][1]);   //1 ->odd -> even  add incoming ele
+        }
+        return max(t[n][0] , t[n][1]);
     }
 };
