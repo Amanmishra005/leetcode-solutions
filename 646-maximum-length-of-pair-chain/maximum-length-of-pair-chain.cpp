2cce1a133f108c74692901232a9bc50254892def
Aman Mishra <amanmishra2884@gmail.com>
2026-07-24 00:33:47 +0530

    Time: 36 ms (49.08%) | Memory: 27.4 MB (33.33%) - LeetSync

--- 646-maximum-length-of-pair-chain/maximum-length-of-pair-chain.cpp
+++ 646-maximum-length-of-pair-chain/maximum-length-of-pair-chain.cpp
@@ -1,22 +1,19 @@
 class Solution {
 public:
-int n;
-int dp[1001][1001];
-int mlpc(vector<vector<int>>& pairs , int prevele , int curridx){
-    if(curridx>=n)return 0;
-    if(prevele != -1 && dp[prevele][curridx]!=-1) return dp[prevele][curridx];
-    int take = 0;
-    if(prevele == -1 || pairs[prevele][1] < pairs[curridx][0]){
-        take = 1 + mlpc(pairs , curridx , curridx+1);
-    }
-    int skip = mlpc(pairs , prevele , curridx+1);
-    if(prevele != -1) dp[prevele][curridx] = max(take ,skip);
-    return  max(take , skip);
-}
     int findLongestChain(vector<vector<int>>& pairs) {
-          n = pairs.size();
-          memset(dp , -1 , sizeof(dp));
-        sort(pairs.begin() , pairs.end());
-        return mlpc(pairs , -1 , 0);
+        int n = pairs.size();
+        sort(pairs.begin(),pairs.end());
+        vector<int> dp(n , 1);
+        int mlpc = 1;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(pairs[j][1] < pairs[i][0]){
+                    dp[i] = max(dp[i] , dp[j] + 1);
+                    mlpc = max(mlpc , dp[i]);
+                }
+            }
+        }
+        return mlpc;
+    
     }
 };
