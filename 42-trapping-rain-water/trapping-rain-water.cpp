47e3f48b735c8e5683f8d2807a0aac8d701834b8
Aman Mishra <amanmishra2884@gmail.com>
2026-07-26 21:29:10 +0530

    Time: 3 ms (16.47%) | Memory: 27.2 MB (34.94%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,36 +1,31 @@
 class Solution {
 public:
+    vector<int> leftidx(vector<int>& height , int &n){
+        vector<int> leftmax(n);
+        leftmax[0] = height[0];
+        for(int i=1;i<n;i++){
+            leftmax[i] = max(leftmax[i-1] , height[i]);
+        }
+        return leftmax;
+    }
+    vector<int> rightidx(vector<int>& height , int &n){
+        vector<int> rightmax(n);
+        rightmax[n-1] = height[n-1];
+        for(int i=n-2;i>=0;i--){
+            rightmax[i] = max(rightmax[i+1] , height[i]);
+        }
+        return rightmax;
+
+    }
     int trap(vector<int>& height) {
-        int n = height.size();
-        if (n == 0) return 0;
-
-        // prev greatest element array (left max)
-        vector<int> prev(n);
-        prev[0] = -1;
-        int maxLeft = height[0]; //max height from left till now
-        for (int i = 1; i < n; i++) {
-            prev[i] = maxLeft;
-            maxLeft = max(maxLeft, height[i]);
+        int n  = height.size();
+        vector<int> leftindx =  leftidx(height , n);
+        vector<int> rightindx = rightidx(height , n);
+        int sum = 0;
+        for(int i=0;i<n;i++){
+            int he= min(rightindx[i] , leftindx[i]) - height[i];
+            sum += he;
         }
-
-        // next greatest element array (right max)
-        vector<int> next(n);
-        next[n - 1] = -1; 
-        int maxRight = height[n - 1];   //max height from right till now
-        for (int i = n - 2; i >= 0; i--) {
-            next[i] = maxRight;
-            maxRight = max(maxRight, height[i]);
-        }
-
-        // calculating trapped water
-        int water = 0;
-        for (int i = 1; i < n - 1; i++) {
-            int minHeight = min(prev[i], next[i]);
-            if (height[i] < minHeight) {
-                water += minHeight - height[i];
-            }
-        }
-
-        return water;
+        return sum;
     }
 };
