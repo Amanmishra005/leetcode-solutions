5147abd2317f66cce3103a3a304b3b823c45ca2a
Aman Mishra <amanmishra2884@gmail.com>
2026-08-11 01:02:28 +0530

    Time: 0 ms (100.00%) | Memory: 35.8 MB (75.13%) - LeetSync

--- 1138-grumpy-bookstore-owner/grumpy-bookstore-owner.cpp
+++ 1138-grumpy-bookstore-owner/grumpy-bookstore-owner.cpp
@@ -2,42 +2,34 @@ class Solution {
 public:
     int maxSatisfied(vector<int>& customers, vector<int>& grumpy, int minutes) {
         int n = customers.size();
-        int k = minutes;
-        int prevLoss = 0;
-
-        // Initial loss window
-        for (int i = 0; i < k; i++) {
-            if (grumpy[i] == 1) prevLoss += customers[i];
+        int currentWind = 0;
+        for(int i=0;i<minutes;i++){
+            if(grumpy[i]==1){
+                currentWind += customers[i];
+            }
         }
-
-        int maxLoss = prevLoss;
-        int maxIdx = 0;
-        int i = 1, j = k;
-
-        while (j < n) {
-            //
-
-            if (grumpy[j] == 1) prevLoss += customers[j];
-            if (grumpy[i - 1] == 1) prevLoss -= customers[i - 1];
-
-            if (prevLoss > maxLoss) {
-                maxLoss = prevLoss;
-                maxIdx = i;
-            }            
-            i++;
-            j++;
+        int maxUnsatisfied = currentWind;
+        int i =0;
+        int j = minutes; //each limit window;
+        while(j < n){
+            if(grumpy[j]==1){
+                currentWind += customers[j]; //new element of new window --> inclusion
+            }
+            if(grumpy[i]==1){
+                currentWind -= customers[i];    //leaving window
+            }
+               
+                maxUnsatisfied = max(maxUnsatisfied , currentWind);
+                i++;
+                j++;
+            }
+        
+        int total  = maxUnsatisfied; //satisfying by calm
+        for(int i=0;i<n;i++){
+            if(grumpy[i]==0){
+                total += customers[i];
+            }
         }
-        // remove grumpiness in best window
-        for (int i = maxIdx; i < maxIdx + k && i < n; i++) {
-            grumpy[i] = 0;  //become non-grumpy -> max customers
-        }
-
-        // Calculate total satisfied customers
-        int total = 0;
-        for (int i = 0; i < n; i++) {
-            if (grumpy[i] == 0) total += customers[i];
-        }
-
-        return total;  
+        return total;
     }
 };
