7e91f89a6b2ae2aca422b956e2a27a26e718a364
Aman Mishra <amanmishra2884@gmail.com>
2026-08-24 01:07:56 +0530

    Time: 2 ms (15.12%) | Memory: 13.8 MB (48.98%) - LeetSync

--- 443-string-compression/string-compression.cpp
+++ 443-string-compression/string-compression.cpp
@@ -2,25 +2,25 @@ class Solution {
 public:
     int compress(vector<char>& chars) {
         int n = chars.size();
-        int index = 0;
         int i = 0;
+        int index = 0;
         while(i<n){
-            char current = chars[i]; 
+            char current = chars[i];
             int count = 0;
-            while(i<n && current==chars[i]){  //same char then count increase 
+            while(i<n && current==chars[i]){
                 count++;
                 i++;
             }
-            chars[index] = current; //store curr char at that index 
+            chars[index] = current;
             index++;
             if(count>1){
-                string count_str = to_string(count);    //if 12 -> "1","2" then store it also
+                string count_str = to_string(count);
                 for(char& ch : count_str){
-                    chars[index] = ch;                      //at index 
-                    index++;                                //move for the next current char 
+                    chars[index] = ch;
+                    index++;
                 }
             }
         }
-        return index;                                   //it will reach the end that is thee compressed size 
+        return index;
     }
 };
