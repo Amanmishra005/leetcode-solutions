9d14ec23cee6d11be7a10cb19e0e327208d0887e
Aman Mishra <amanmishra2884@gmail.com>
2026-08-24 20:18:12 +0530

    Time: 15 ms (49.18%) | Memory: 23.2 MB (95.62%) - LeetSync

--- 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.cpp
+++ 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.cpp
@@ -1,33 +1,26 @@
 class Solution {
 public:
-    bool closeStrings(string str1, string str2) {
-        if(str1.length()!=str2.length()) return false;
-        unordered_map<char,int> mp1,mp2;
-        for(int i=0;i<str1.length();i++){
-            mp1[str1[i]]++;              //catching char of strings 
-            mp2[str2[i]]++;
-        }
-        //compare chars
-        for(auto x : mp1){
-            char ch = x.first;       //i.e char 
-            if(mp2.find(ch)==mp2.end()) return false;
-        }
-        //now frequency of each char 
-        unordered_map<int,int> h1,h2; 
-        for(auto x : mp1){
-            int frq = x.second;
-            h1[frq]++;         //filling up the freq     
-        }
-        for(auto x: mp2){
-            int frq = x.second;
-            h2[frq]++;
-        }
-        //compare on frq
-        for(auto x: h1){
-            int key = x.first;
-            if(h2.find(key)==h2.end()) return false;    //found 
-            if(h1[key]!=h2[key]) return false;      //compare freq
-        }
-        return true;
+    bool closeStrings(string word1, string word2) {
+       int m = word1.size();
+       int n = word2.size();
+       if(m != n) return false;
+       vector<int> freq1(26);          //a, b, c , .... z for filling occurene
+       vector<int> freq2(26);
+       for(int i=0;i<m;i++){        //since m && n should be equal so both work in one loop we can 
+        char ch1 = word1[i];
+        char ch2 = word2[i];
+        int idx1 = ch1 - 'a';      //c - a = 2 so at 2 index fill 1 i.e c occured one time till now
+        int idx2 = ch2 - 'a';
+        freq1[idx1]++;              //occurence increase
+        freq2[idx2]++;
+       }
+       for(int i=0;i<26;i++){
+        if(freq1[i] != 0 && freq2[i] != 0) continue;       //without this exchnage not possible i.e existence 
+        if(freq1[i] == 0 && freq2[i] == 0) continue;       //exchange with only the available present char 
+        return false;
+       }
+       sort(freq1.begin() , freq1.end());
+       sort(freq2.begin() , freq2.end());
+        return freq1==freq2;  //if freq same then exchange with those char possible 
     }
-};  
+};
