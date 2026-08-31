2aea9044b53804b00809c795becf83dc402beb57
Aman Mishra <amanmishra2884@gmail.com>
2026-08-31 13:42:47 +0530

    Time: 0 ms (100.00%) | Memory: 124.7 MB (44.10%) - LeetSync

--- 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
+++ 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
@@ -1,37 +1,51 @@
-
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     vector<int> nodesBetweenCriticalPoints(ListNode* head) {
-        int idx =1;
-        int fidx = -1;                      //firstindex ->left
-        int sidx = -1;                      //lastindex ->right
-        ListNode* a = head;
-        ListNode* b = head->next;
-        ListNode* c = head->next->next;
-        if(c==NULL) return {-1,-1};
-        //for min
-        int f =-1;
-        int s =-1;
-        int mind = INT_MAX;
-        while(c){
-            if(b->val > a->val && b->val > c->val || b->val < a->val && b->val < c->val){               //maxd
-                if(fidx==-1) fidx = idx;
-                else sidx = idx;
-                //mindistance
-                f= s;
-                    s = idx;
-                    if(f!=-1){
-                        int d = s - f;
-                    mind = min(mind,d);
-                    }
+        ListNode* temp = head;
+        ListNode* tempn = head->next;
+        ListNode* tempnn = head->next->next;
+        int idx = 1;        //curr idx point
+        int firstidx = -1;      //to find max dis 
+        int secidx = -1;
+
+        if(tempnn==NULL) return {-1,-1};
+        int prevCp = -1; //to find min dis 
+        int currCp = -1;
+        int minimumdis = INT_MAX;
+
+        while(tempnn){
+            if(tempn->val > temp->val && tempn->val > tempnn->val || tempn->val < temp->val && tempn->val < tempnn->val){
+                if(firstidx == -1) firstidx = idx;
+                else secidx = idx;
+
+                prevCp = currCp;
+                currCp  = idx;
+
+                if(prevCp != -1){
+                    int cpdis  = currCp - prevCp;
+                    minimumdis = min(minimumdis , cpdis);
+                }
+
             }
-            a=a->next;
-            b = b->next;
-            c = c->next;
+            temp = temp->next;
+            tempn = tempn->next;
+            tempnn = tempnn ->next;
             idx++;
         }
-        if(sidx==-1) return {-1,-1}; // one critical point then return vector -1,-1
-        int maxd = sidx-fidx;
-        return{mind,maxd};
+        if(secidx == -1) return {-1,-1}; //if only one critical tab return kardo -1 -1
+        int maxdis = secidx - firstidx;
+        return {minimumdis , maxdis};
+
+    
     }
 };
